ff2999f0e1bb576701a09ab09d28dfe39c736b75
grigoriu-stefan-iulian <grigoriu.stefan.iulian@gmail.com>
2024-03-04 20:48:35 +0200

    update vite env

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   const [data, setData] = useState("");
 
   useEffect(() => {
-    fetch(`${process.env.REACT_APP_API_URL}/api/products/`)
+    fetch(`${import.meta.env.VITE_API_URL}/api/products/`)
       .then((res) => res.json())
       .then((data) => setData(JSON.stringify(data)));
   }, []);
